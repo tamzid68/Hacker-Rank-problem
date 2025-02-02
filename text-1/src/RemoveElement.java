065550aebb34dbaf05f8e82c0250f0780e47553c
ASM Tamzid <atkthegreat9999@gmail.com>
2025-02-02 15:47:28 +0600

    added Remove Element

---
+++ text-1/src/RemoveElement.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static com.sun.org.apache.bcel.internal.classfile.Utility.printArray;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 6, 3, 2, 3};
+        int val = 3;
+
+        int newLength = removeElement(nums, val);
+
+        System.out.println(Arrays.toString(Arrays.copyOf(nums, newLength)));
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val)
+                nums[count++] = nums[i];
+
+        }
+        return count;
+    }
+}
+
